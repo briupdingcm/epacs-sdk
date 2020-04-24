aa9878e224dab86be0d5128d11826506993757ba
briupdingcm <dingcm@briup.com>
2020-04-24 15:23:23 +0800

    initial commit

--- src/test/java/com/epacs/sdk/common/ResponseTest.java
+++ src/test/java/com/epacs/sdk/common/ResponseTest.java
@@ -1,44 +1,67 @@
 package com.epacs.sdk.common;
 
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
-public class ResponseTest extends TestCase {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResponseTest{
     String jsonStr = "{\"log_id\":\"1\", \"error_code\":\"200\", \"error_msg\":\"hello\"}";
 
     String errJsonStr = "{\"log_id\":\"\", \"error_code\":\"200\", \"error_msg\":\"hello\"}";
 
     Response response;
     Response errResponse;
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        response = new Response(jsonStr);
-        errResponse = new Response(errJsonStr);
+
+
+    @Test
+    public void testConstructor()  {
+        try {
+            assertNotNull(new Response(jsonStr));
+        } catch (ResponseException e) {
+            fail("");
+        }
     }
 
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
+    @Test
+    public void testConstructorEx() {
+        try {
+            new Response(errJsonStr);
+        } catch (ResponseException e) {
+            ;
+        }
     }
 
-    public void testResponse() throws ResponseException {
-        Response response = new Response(jsonStr);
-        assertNotNull(response);
-    }
 
+    @Test
     public void testGetLogId(){
+        try {
+            response = new Response(jsonStr);
+        } catch (ResponseException e) {
+            fail(e);
+        }
         assertEquals(new Integer(1), response.getLogId());
     }
 
-    public void testGetLogIdNull(){
-        assertNull(errResponse.getLogId());
-    }
 
+
+    @Test
     public void testGetErrorCode(){
+        try {
+            response = new Response(jsonStr);
+        } catch (ResponseException e) {
+            fail(e);
+        }
         assertEquals(new Integer(200), response.getErrorCode());
     }
 
+    @Test
     public void testGetErrorMsg(){
+        try {
+            response = new Response(jsonStr);
+        } catch (ResponseException e) {
+            fail(e);
+        }
         assertEquals("hello", response.getErrorMsg());
     }
 }
